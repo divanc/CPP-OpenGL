bff985dc4c17748d105200006a50e0f8f9591c48
Ivan <DivanDoesApps@gmail.com>
2022-06-04 19:44:08 +0300

    Initial Commit

---
+++ OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL
+//
+//  Created by Ivan on 04.06.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
